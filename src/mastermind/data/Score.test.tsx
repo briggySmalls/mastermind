f97b8933e7f7a1b12dd50dc3d929a5bc7b288de8
Sam Briggs <samuel.briggs@depop.com>
2022-02-17 11:17:06 +0100

    Parameterise tests in jest

--- src/mastermind/data/Score.test.tsx
+++ src/mastermind/data/Score.test.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { calculateScore } from './Score';
 
-test('calculates score correctly', () => {
-  const params = [
-    {guess: "yybgg", answer: "ygybp", score: {exact: 1, partial: 3}},
-    {guess: "yybbg", answer: "ygybp", score: {exact: 2, partial: 2}},
-    {guess: "yybgp", answer: "ygybp", score: {exact: 2, partial: 3}},
-    {guess: "yyyyy", answer: "ygybp", score: {exact: 2, partial: 0}},
-  ]
-  params.forEach(({guess, answer, score}) => {
+describe('calculates score correctly', () => {
+  it.each([
+    ["yybgg", "ygybp", {exact: 1, partial: 3}],
+    ["yybbg", "ygybp", {exact: 2, partial: 2}],
+    ["yybgp", "ygybp", {exact: 2, partial: 3}],
+    ["yyyyy", "ygybp", {exact: 2, partial: 0}],
+    ["rrgg", "ypyp", {exact: 0, partial: 0}],
+    ["yypp", "ypyp", {exact: 2, partial: 2}],
+    ["gypr", "ypyp", {exact: 0, partial: 2}],
+  ])(
+    `when guessing '%s' for answer '%s' should calculate score %o`,
+    (guess, answer, score) => {
     expect(calculateScore(answer, guess)).toEqual(score);
   })
 });
