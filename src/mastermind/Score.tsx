a8f9933d688c9574bb59e1f5462a456c010ab9cb
Sam Briggs <samuel.briggs@depop.com>
2022-02-20 09:23:45 +0100

    Add some styling to scoreboard

--- src/mastermind/Score.tsx
+++ src/mastermind/Score.tsx
@@ -8,21 +8,27 @@ interface ScoreProps {
 const markerWidth = 1;
 
 const ScoreBoard = styled.div`
-margin-left: 1em;
-width: ${markerWidth * 5}em;
+  margin-left: 1em;
+  width: ${markerWidth * 2}em;
+  height: ${markerWidth * 2}em;
+  border: 0.1em solid white;
+  border-radius: 20%;
+  display: flex;
+  flex-wrap: wrap;
 `
+
 const Match = styled.div`
-display: inline-block;
-width: 1em;
-height: 1em;
-border-radius: 50%;
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
 `
 
 const ExactMatch = styled(Match)`
-background-color: red;
+  background-color: red;
 `
 const PartialMatch = styled(Match)`
-background-color: white;
+  background-color: white;
 `
 
 function ScoreComponent({ score }: ScoreProps) {
